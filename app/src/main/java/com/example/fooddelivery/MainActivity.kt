60f24fcafff3199777505a2c617f4ee3cfc4f6ef
Alexander <a_36524@mail.ru>
2024-11-02 10:44:25 +0400

    тест MVVM

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -5,40 +5,90 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.fooddelivery.ui.theme.FoodDeliveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Greeting()
+            val viewModel: CounterViewModel = viewModel()
+            FoodDeliveryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(viewModel)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Greeting() {
+fun Greeting(viewModel: CounterViewModel) {
     /*Lazy*/Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
+//        val count = remember {
+//            mutableStateOf(0)
+//        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Count ${viewModel.count.value}",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Button(onClick = { viewModel.increment() }) {
+                    Text(text = "increment")
+                }
+                Button(onClick = { viewModel.decrement() }) {
+                    Text(text = "decrement")
+                }
+            }
+
+        }
+
+
+
+
+
+
+
 //        гамбургер и адрес
         Row {
             IconButton(onClick = { /*TODO*/ }) {
-                
+
             }
             Column {
                 Text(text = "Доставка")
@@ -52,7 +102,7 @@ fun Greeting() {
         Row {
 //            SearchBar(query = , onQueryChange = , onSearch = , active = , onActiveChange = ) {
                 IconButton(onClick = { /*TODO*/ }) {
-                    
+
                 }
         }
 //        Промо разделы
@@ -187,8 +237,8 @@ fun Greeting() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-        Greeting()
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//        Greeting(viewModel)
+//}
